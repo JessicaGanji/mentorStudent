5dabc0bc8273abfd2b7f1fbf073f51988a23b94c
TJ Palazzari <tjpalazzari@yahoo.com>
2016-01-22 10:26:02 -0800

    adds to index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var ejsLayouts   = require("express-ejs-layouts");
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
+var path		 = require('path');
 var session      = require('express-session');
 var port 		 = process.env.PORT || 3000;
 var mongoUri   	 = process.env.MONGOLAB_URI || 'mongodb://localhost/project_three';
@@ -14,4 +15,19 @@ var mongoUri   	 = process.env.MONGOLAB_URI || 'mongodb://localhost/project_thre
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+app.set('views', path.join(__dirname, 'views'))
+app.engine('ejs', require ('ejs').renderFile)
+app.set('view engine', 'ejs')
+
+app.use(express.static(__dirname + '/public'))
+
+app.use(session({ secret: 'WDI-GENERAL-ASSEMBLY-EXPRESS' })); 
+app.use(passport.initialize());
+app.use(passport.session()); 
+app.use(flash()); 
+
+require('./config/passport')(passport);
+
+mongoose.connect(mongoUri)
 
