39292b92629e66fbca04066cc0c5b17a3d45c4cc
Jessica Ganji <jessicaganji@gmail.com>
2016-01-22 10:36:36 -0800

    creates basic models

---
+++ models/mentor.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+var MentorSchema = mongoose.Schema({
+	local: {
+		email        : String,
+		password     : String,
+	}
+	first_name: String,
+	last_name: String,
+	headline: String,
+	location: String,
+	area_of_expertise: String,
+	bio: String,
+	personal_website: String,
+	compary_website: String,
+	experiences: String,
+	skills: String,
+	availibility: String,
+	status: Boolean,
+	terms: Boolean
+});
+
+Mentor.methods.encrypt = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+Mentor.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('Mentor', MentorSchema);
