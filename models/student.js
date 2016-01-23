8039b5134a6019301ce58b72fee5b2fed2a34947
TJ Palazzari <tjpalazzari@yahoo.com>
2016-01-22 16:47:59 -0800

    fixes models

--- models/student.js
+++ models/student.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var bcrypt   = require('bcrypt-nodejs');
 
 var Student = mongoose.Schema({
-	student-local: {
+	local: {
 		email        : String,
 		password     : String,
 	},
