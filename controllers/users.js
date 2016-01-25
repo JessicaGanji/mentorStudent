679fe3db67449f541412001f561b7d5478b3b8c7
Jessica Ganji <jessicaganji@gmail.com>
2016-01-25 11:06:14 -0800

    adds to user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-var Student   = require('../models/user.js')
+var User      = require('../models/user.js')
 var passport  = require('passport')
 
 // Mentors
@@ -49,82 +49,65 @@ function getIndex(request, response) {
 
 // GET /mentors/:id
 function getProfile(request, response) {
+  var id = request.params.id;
+
+  User.findById({ _id: id}, function (error, user){
+    if(error) response.json({ message: "There is an error on this page because:" + error });
+    response.json({ user: user })
+  }
+
   response.render('mentors/profile.ejs');
 }
 
 // GET /mentors/edit
 function getEdit(request, response) {
+
+
   response.render('mentors/edit.ejs');
 }
 
 // PATCH mentors
 function patchProfile(request, response) {
-  response.redirect('/');
+  var id = request.params.id;
+
+  User.findById({ _id: id }, function (error, user){
+    if(error) response.json({ message: "There is an error on this page becuase:" + error });
+
+    if(request.body.first_name) user.first_name = request.body.first_name;
+    if(request.body.last_name) user.last_name = request.body.last_name;
+    if(request.body.headline) user.headline = request.body.headline;
+    if(request.body.location) user.location = request.body.location;
+    if(request.body.focus) user.focus = request.body.focus;
+    if(request.body.intro) user.intro = request.body.intro;
+    if(request.body.company) user.company = request.body.company;
+    if(request.body.company_website) user.company_website = request.body.company_website;
+    if(request.body.education) user.experiences = request.body.experiences;
+    if(request.body.personal_website) user.personal_website = request.body.personal_website;
+    if(request.body.experiences) user.experiences = request.body.experiences;
+    if(request.body.skills) user.skills = request.body.skills;
+    if(request.body.availibility) user.availibility = request.body.availibility;
+    if(request.body.status) user.status = request.body.status;
+    if(request.body.terms) user.terms = request.body.terms;
+
+    user.save( function (error){
+      if(error) response.json({ message: "Could not save user becuase:" + error });
+      response.json({ message: "User has been updated!" });
+    })
+
+  })
+  
+  response.redirect('mentors/profile.ejs');
 }
 
 // DELETE mentors
 function deleteProfile(request, response) {
-  response.redirect('/');
-}
+  var id = request.params.id;
 
-// Students
-// GET students/signup
-function getStudentSignup(request, response) {
-  response.render('mentors/signup.ejs', { message: request.flash('signupMessage') });
-}
+  User.remove({_id: id}, function (error) {
+    if(error) response.json({ message: "User has not been deleted due to the following error:" + error });
+    response.json({ message: "User has been deleted." })
+  })
 
-// POST students/signup
-function postStudentSignup(request, response) {
-  console.log(request.params)
-
-  var signUpStrategy = passport.authenticate('local-signup', {
-    successRedirect : '/', 
-    failureRedirect : '/signup', 
-    failureFlash : true 
-  });
-
-  return signUpStrategy(request, response) 
-}
-
-// GET students/login
-function getStudentLogin(request, response) { 
-  response.render('mentors/login.ejs', { message: request.flash('loginMessage') }); 
-}
-
-// POST students/login 
-function postStudentLogin(request, response) {
-  var loginProperty = passport.authenticate('local-login', {
-    successRedirect : '/', 
-    failureRedirect : '/login', 
-    failureFlash : true 
-  });
-
-  return loginProperty(request, response);
-}
-
-// GET students/logout
-function getStudentLogout(request, response) {
-  request.logout();
-  response.redirect('/');
-}
-
-// GET /students/:id
-function getStudentProfile(request, response) {
-  response.render('students/profile.ejs');
-}
-
-// GET /students/edit
-function getStudentEdit(request, response) {
-  response.render('students/edit.ejs');
-}
-
-// PATCH student
-function patchStudentProfile(request, response) {
-  response.redirect('/');
-}
-
-// DELETE student
-function deleteStudentProfile(request, response) {
   response.redirect('/');
 }
 
