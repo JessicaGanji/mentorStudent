831011eafe16fede53e11c9707147d5308cd3d85
TJ Palazzari <tjpalazzari@yahoo.com>
2016-03-01 22:38:35 -0800

    adds copy to index.ejs

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-,var express 				= require('express');
-var app							= express();
-var mongoose				= require('mongoose');
-var passport 				= require('passport');
-var flash 					= require('connect-flash');
-var ejsLayouts 			= require('express-ejs-layouts');
-var logger 					= require('morgan');
-var cookieParser 		= require('cookie-parser');
-var bodyParser 			= require('body-parser');
-var path		   			= require('path');
-var session 				= require('express-session');
-var aws							= require('aws-sdk')
+var express                 = require('express');
+var app                         = express();
+var mongoose                = require('mongoose');
+var passport                = require('passport');
+var flash                   = require('connect-flash');
+var ejsLayouts          = require('express-ejs-layouts');
+var logger                  = require('morgan');
+var cookieParser        = require('cookie-parser');
+var bodyParser          = require('body-parser');
+var path                    = require('path');
+var session                 = require('express-session');
+var aws                         = require('aws-sdk')
 var methodOverride  = require('method-override');
 var AWS_ACCESS_KEY  = process.env.AWS_ACCESS_KEY;
 var AWS_SECRET_KEY  = process.env.AWS_SECRET_KEY;
 var S3_BUCKET       = process.env.S3_BUCKET;
-var port 		    		= process.env.PORT || 3000;
-var mongoUri 	    	= process.env.MONGOLAB_URI || ('mongodb://localhost/project_three');
+var port                    = process.env.PORT || 3000;
+var mongoUri            = process.env.MONGOLAB_URI || 'mongodb://localhost/project_three';
 
-var staticRouter  	= require('./config/routes/static_routes.js');
-var userRouter			= require('./config/routes/user_routes.js');
-var userPassport		= require('./config/passport.js');
-var userSetUp				= userPassport(passport);
-var resourceRouter 	= require('./config/routes/resource_routes.js');
-var apiRouter				= require('./config/routes/api_routes.js');
+var staticRouter    = require('./config/routes/static_routes.js');
+var userRouter          = require('./config/routes/user_routes.js');
+var userPassport        = require('./config/passport.js');
+var userSetUp               = userPassport(passport);
+var resourceRouter  = require('./config/routes/resource_routes.js');
+var apiRouter               = require('./config/routes/api_routes.js');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -46,7 +46,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(session({ secret: 'WDI-GENERAL-ASSEMBLY-EXPRESS' })); 
 app.use(passport.initialize());
 app.use(passport.session()); 
-app.use(flash());
+app.use(flash()); 
 
 mongoose.connect(mongoUri);
 
@@ -90,4 +90,3 @@ app.use('/', apiRouter);
 
 app.listen(port);
 console.log('Magic is happening on port' + port);
-
